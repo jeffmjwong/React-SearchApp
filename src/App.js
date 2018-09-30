fddaa658bd96922f2e65b9ffa14bbae2dc02bb4c
Jeff Wong <jeffmjwong@gmail.com>
2018-09-30 18:28:38 +1000

    Install react bootstrap and used components from there

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grid, Jumbotron } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Jumbotron>
+          <Grid>
+            <h1>Search App</h1>
+            <p>This is a simple search app!</p>
+          </Grid>
+        </Jumbotron>;
       </div>
     );
   }
